1db62ba79f1adc9ce65b9bc46e5e8b5730446ad6
Cvetan Djikovski <cvetan.djikovski@atlanticgrupa.com>
2022-09-21 15:42:18 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Zdravo Svetu!'));
+app.get('/', (req, res) => res.send('DA VIDIME STA KAZE'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
